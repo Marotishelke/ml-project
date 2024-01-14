2a5953748e995ead68880150b736c9301020f61b
Marotishelke <marotishelke2000@gmail.com>
2024-01-15 00:39:37 +0530

    Exception file Updated

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _, _, exc_tab = error_detail.exc_info()
@@ -17,3 +17,10 @@ class CustomException(Exception):
         
     def __str__(self):
         return self.error_message
+    
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.info("Divide By Zero....")
+        CustomException(e, sys)
